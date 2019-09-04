d2895a4ecf441d4fd5bd1c7d8f9a64b545f28fbb
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 21:22:03 -0400

    change serverpath for heroku

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,6 +1,6 @@
 
 const config = {
-    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'ec2-34-213-171-135.us-west-2.compute.amazonaws.com:3001'
+    baseApiUri: 'http://localhost:3001' //process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'ec2-34-213-171-135.us-west-2.compute.amazonaws.com:3001'
 };
 
 export default config;
