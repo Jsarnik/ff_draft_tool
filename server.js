49395209479836b99e209f8ca24fcf3ff060f4ff
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 21:15:26 -0400

    change cors

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(timeout(120000));
 app.use(haltOnTimedout);
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', `*`);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use(express.static(path.join(__dirname,'/app/build')));
 routes.configure(app);
