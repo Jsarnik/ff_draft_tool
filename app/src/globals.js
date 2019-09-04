54b5e22d04154f5a61b80b7974577148808cf9f4
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 21:35:33 -0400

    dns change again

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,5 +1,5 @@
 const config = {
-    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://localhost:3001'
+    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://espn-fantasy-tool.herokuapp.com/'
 };
 
 export default config;
