5dc6197dfca804ebe3a85d0f6da91c72d535e704
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 22:17:01 -0400

    change dns to https

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,5 +1,5 @@
 const config = {
-    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://espn-fantasy-tool.herokuapp.com/'
+    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://espn-fantasy-tool.herokuapp.com/'
 };
 
 export default config;
