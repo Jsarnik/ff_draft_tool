37ac25e47c020779d8cca40060778ef0531ac147
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 21:29:19 -0400

    run build to change global dns

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,6 +1,5 @@
-
 const config = {
-    baseApiUri: 'http://localhost:3001' //process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'ec2-34-213-171-135.us-west-2.compute.amazonaws.com:3001'
+    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://localhost:3001'
 };
 
 export default config;
