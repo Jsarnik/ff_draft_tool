e776b16ca7f12985c662b7d9673a50bedab3fda2
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-09-03 20:20:26 -0400

    wrong ec2 instance

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,6 +1,6 @@
 
 const config = {
-    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'ec2-34-216-74-242.us-west-2.compute.amazonaws.com:3001'
+    baseApiUri: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'ec2-34-213-171-135.us-west-2.compute.amazonaws.com:3001'
 };
 
 export default config;
